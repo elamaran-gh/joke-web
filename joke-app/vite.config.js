20a915811c7879eff742cbf164b3a3b23beb512e
elamaran-gh <elamaran1928@gmail.com>
2026-05-21 12:12:49 +0530

    fix: clean vite config and vercel.json

--- joke-app/vite.config.js
+++ joke-app/vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-import Path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": Path.resolve("")
-    }
-  }
 })
